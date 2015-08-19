34bbfab0808c759348ce9e415ef0d00bf3c86713
msimons <marco@imerce.nl>
2015-08-19 10:35:37 +0200

    Only log memory footprint.

--- src/main/java/org/xbib/elasticsearch/plugin/jdbc/util/MemoryManagementUtil.java
+++ src/main/java/org/xbib/elasticsearch/plugin/jdbc/util/MemoryManagementUtil.java
@@ -9,35 +9,19 @@ public final class MemoryManagementUtil {
     private final static int MB = 1024*1024;
     private final static String NEWLINE = System.getProperty("line.separator");
 
-    private MemoryManagementUtil() {};
+    private MemoryManagementUtil() {}
 
     public static void logMemoryStatistics() {
         Runtime runtime = Runtime.getRuntime();
 
-        StringBuilder sb = new StringBuilder(NEWLINE);
-        sb.append("## Heap memory statistics [MB] ##").append(NEWLINE);
-        sb.append("Used Memory:").append((runtime.totalMemory() - runtime.freeMemory()) / MB).append(NEWLINE);
-        sb.append("Free Memory:").append(runtime.freeMemory() / MB).append(NEWLINE);
-        sb.append("Total Memory:").append(runtime.totalMemory() / MB).append(NEWLINE);
-        sb.append("Max Memory:").append(runtime.maxMemory() / MB).append(NEWLINE);
-
-        logger.debug(sb.toString());
+        logger.debug(NEWLINE + "## Heap memory statistics [MB] ##" + NEWLINE +
+                "Used Memory:" + (runtime.totalMemory() - runtime.freeMemory()) / MB + NEWLINE +
+                "Free Memory:" + runtime.freeMemory() / MB + NEWLINE +
+                "Total Memory:" + runtime.totalMemory() / MB + NEWLINE +
+                "Max Memory:" + runtime.maxMemory() / MB + NEWLINE);
     }
 
     public static void freeMemoryCheckup() {
-//        try {
-//            if (Runtime.getRuntime().freeMemory() / MB < 100) {
-//                logger.info("Free memory too low. Waiting for free memory... (100 MB expected)");
-//                while (Runtime.getRuntime().freeMemory() / MB < 100) {
-//                    Thread.sleep(100);
-//                    System.gc();
-//                    logger.debug("Current free memory: {} MB", Runtime.getRuntime().freeMemory() / MB);
-//                }
-//                logger.debug("Free memory size back to minimal 100 MB.");
-//            }
-//        } catch (InterruptedException e) {
-//            logger.error("Interrupted sleep");
-//        }
         System.gc();
         logger.debug("Current free memory: {} MB", Runtime.getRuntime().freeMemory() / MB);
     }
