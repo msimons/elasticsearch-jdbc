c78602501ac79bf2c2bb4078635488c3a0803f71
msimons <marco@imerce.nl>
2015-09-29 13:26:05 +0200

    Support for partial document updates

--- src/main/java/org/xbib/elasticsearch/common/util/SinkKeyValueStreamListener.java
+++ src/main/java/org/xbib/elasticsearch/common/util/SinkKeyValueStreamListener.java
@@ -61,7 +61,9 @@ public class SinkKeyValueStreamListener<K, V> extends PlainKeyValueStreamListene
                 output.index(object, false);
             } else if ("index".equals(object.optype())) {
                 output.index(object, false);
-            } else if ("create".equals(object.optype())) {
+            } else if ("update".equals(object.optype())) {
+                output.index(object, false);
+            }else if ("create".equals(object.optype())) {
                 output.index(object, true);
             } else if ("delete".equals(object.optype())) {
                 output.delete(object);
